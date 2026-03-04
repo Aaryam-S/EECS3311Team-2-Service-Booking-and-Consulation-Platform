35875eb4e41e40aed8b0ac9cb8449ba279d021b3
Aaryam3 <aaryamsharma3@gmail.com>
2026-03-04 10:14:37 -0500

    added service and TimeSlot domain models

--- src/model/Service.java
+++ src/model/Service.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Service {
+    private String name;
+    private double rate;
+    private int duration; // in minutes
 
+    public Service(String name, double rate, int duration) {
+        this.name = name;
+        this.rate = rate;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s ($%.2f) - %d mins", name, rate, duration);
+    }
 }
--- src/model/TimeSlot.java
+++ src/model/TimeSlot.java
@@ -1,5 +1,39 @@
 package model;
 
-public class TimeSlot {
+import java.time.LocalDateTime;
 
+public class TimeSlot {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private boolean isBooked;
+
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("Start time must be before end time");
+        }
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isBooked = false;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    @Override
+    public String toString() {
+        return startTime.toString() + " - " + endTime.toString() + (isBooked ? " (Booked)" : " (Available)");
+    }
 }
