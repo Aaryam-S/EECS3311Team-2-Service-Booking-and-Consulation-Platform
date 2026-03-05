95413e66f9dc5b6a56e636d9787b8136bd9b532e
arshjotsingh <arshjotsingh@localhost>
2026-03-04 23:36:56 -0500

    Add CatalogService to support UC1 (Browse Consulting Services)

---
+++ src/service/CatalogService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+import model.Service;
+
+public class CatalogService {
+
+    public List<Service> browseServices() {
+        return null;
+    }	
+	
+}
