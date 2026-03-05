02f6aec3124de33e9337bdad2bdfefebe147599a
arshjotsingh <arshjotsingh@localhost>
2026-03-05 16:23:28 -0500

    Implement payment processing logic in PaymentService

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -4,6 +4,7 @@ import model.PaymentReceipt;
 import model.SavedPaymentMethod;
 import strategy.PaymentStrategy;
 
+
 public class PaymentService {
 
 	//Constructor
@@ -13,13 +14,55 @@ public class PaymentService {
 	
 	//Methods
 	public PaymentReceipt process(double amount, PaymentStrategy strategy) {
-		// TODO: validate + simulate payment + create receipt 
-		return null;
+		if (strategy == null) {
+			throw new IllegalArgumentException("Payment strategy cant be null.");
+		}
+		if (amount <= 0) {
+			throw new IllegalArgumentException("Amount has to be more than 0");
+		}
+		
+		//Validate details
+        boolean valid = strategy.validateDetails(); 
+        if (!valid) {
+        	//Invalid Exception 
+            return null;
+        }
+        
+        // 2-3 seconds delay
+        simulateDelay(); 
+        
+        boolean success = strategy.processPayment(amount);
+        if (!success) {
+            return null;
+        }
+        
+        // Create receipt (field filling needs to be done) 
+        PaymentReceipt receipt = new PaymentReceipt();
+
+        return receipt;
 	}
 	
 	public PaymentReceipt processWithSavedMethod(double amount, SavedPaymentMethod method) {
-		// TODO
-		return null;
+		
+        if (method == null) {
+            throw new IllegalArgumentException("Saved payment method cannot be null.");
+        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0.");
+        }
+		
+        // Need to implement Mapping SavedPaymentMethod to PaymentStrategy 
+        return null;
 	}
 	
+    //Helper for delay
+    private void simulateDelay() {
+        try {
+            long delayMs = 2000 + (long) (Math.random() * 1000); 
+            Thread.sleep(delayMs);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
 }
