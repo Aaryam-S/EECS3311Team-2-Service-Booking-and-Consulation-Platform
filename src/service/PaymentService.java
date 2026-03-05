ec4b73bb4ce4d6fb13d48991b56f99f72f195ae3
arshjotsingh <arshjotsingh@localhost>
2026-03-05 14:04:18 -0500

    Add UML skeleton implementations for PaymentService

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -1,5 +1,25 @@
 package service;
 
+import model.PaymentReceipt;
+import model.SavedPaymentMethod;
+import strategy.PaymentStrategy;
+
 public class PaymentService {
 
+	//Constructor
+	public PaymentService() {
+		
+	}
+	
+	//Methods
+	public PaymentReceipt process(double amount, PaymentStrategy strategy) {
+		// TODO: validate + simulate payment + create receipt 
+		return null;
+	}
+	
+	public PaymentReceipt processWithSavedMethod(double amount, SavedPaymentMethod method) {
+		// TODO
+		return null;
+	}
+	
 }
