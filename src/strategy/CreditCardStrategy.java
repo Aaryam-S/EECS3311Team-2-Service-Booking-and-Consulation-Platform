5951a37fbaae0ce564393ae12d6fc94455ceb9ee
arshjotsingh <arshjotsingh@localhost>
2026-03-05 13:46:31 -0500

    Add UML skeleton implementations for my strategy classes(attributes and methods)

--- src/strategy/CreditCardStrategy.java
+++ src/strategy/CreditCardStrategy.java
@@ -2,4 +2,28 @@ package strategy;
 
 public class CreditCardStrategy implements PaymentStrategy {
 
+	//Attributes
+	private String cardNumber;
+	private int exipryMonth;
+	private int expiryYear;
+	private String cvv;
+	
+	//Constructor 
+	public CreditCardStrategy() {
+
+	}
+	
+	//Methods 
+	@Override 
+	public boolean validateDetails() {
+		// TODO
+		return true;
+	}
+	
+	@Override 
+	public boolean processPayment(double amount) {
+		// TODO
+		return true;
+	}
+		
 }
--- src/strategy/PayPalStrategy.java
+++ src/strategy/PayPalStrategy.java
@@ -2,4 +2,25 @@ package strategy;
 
 public class PayPalStrategy implements PaymentStrategy {
 
+	//Attributes
+    private String email;
+
+    //Constructor
+    public PayPalStrategy() {
+
+    }
+
+    //Methods
+    @Override
+    public boolean validateDetails() {
+        // TODO
+        return true;
+    }
+
+    @Override
+    public boolean processPayment(double amount) {
+        // TODO
+        return true;
+    }	
+	
 }
